b24616e13292feba914fff7e8cdbae9e877af7a6
Neuheit <38368299+Neuheit@users.noreply.github.com>
2020-02-23 20:34:17 -0500

    Create oracle testing base.

---
+++ DSharpPlus.Oracle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSharpPlus.Oracle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
